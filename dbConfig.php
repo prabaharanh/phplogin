9e64f2b2587af34a6d466d7a4886182ff3be8b31
Prabaharan Haridass <prabaharan.haridass@outlook.com>
2019-08-13 20:10:43 +1200

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -1,9 +1,3 @@
 <?php
-define ("DB_HOST", "phmysqldb.mysql.database.azure.com"); // Your database host name
-define ("DB_USER", "prabaharanh@phmysqldb"); // Your database user
-define ("DB_PASS","Radhah@5969"); // Your database password
-define ("DB_NAME","dblogin"); // Your database name
-
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
-$db = mysqli_select_db($link, DB_NAME) or die("Couldn't select database");
+$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "phmysqldb.mysql.database.azure.com", "prabaharanh@phmysqldb", {Radhah@5969}, {dblogin}, 3306);
 ?>
