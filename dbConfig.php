bf58a448ecbd3a34f81e579b84c5b9a5ca61922a
Prabaharan Haridass <prabaharan.haridass@outlook.com>
2019-08-13 20:14:35 +1200

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "phmysqldb.mysql.database.azure.com", "prabaharanh@phmysqldb", {Radhah@5969}, {dblogin}, 3306);
+$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "phmysqldb.mysql.database.azure.com", "prabaharanh@phmysqldb", "Radhah@5969", "dblogin", 3306);
 ?>
